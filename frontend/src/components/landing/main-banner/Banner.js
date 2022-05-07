5e7195dd65026e44f9117d2ff7b608258d9314a9
SardarAhmed786 <sardarahmadnaseem@gamil.com>
2022-05-07 18:24:48 +0500

    changes

--- frontend/src/components/landing/main-banner/Banner.js
+++ frontend/src/components/landing/main-banner/Banner.js
@@ -20,12 +20,13 @@ const Banner = () => {
                     <Navbar />
                     <div className="container">
                         <div className="row ptb">
-                            <div className="col-sm-12">
+                            <div className="col-sm-6">
                                 <div className="inner-content text-center">
                                     <div className="inner-img ">
-                                        <h6 className="common mb-3">Welcome to <b>Projectstarter</b> </h6>
-                                        <h1 className=""> Projects That Even We Would Invest In  <span className="common ">Making NFTs & IDO Pre-Sales accessible to first-timers and truly exciting for savvy investors</span></h1>
-                                        <p className="ptb20 grey">Get started with as little as 1000 PSR</p>
+                                      
+                                        <h1 className=""> Projects That Even  </h1>
+                                        <h1 className=''>We Would Invest In</h1>
+                                       
                                         <ul className="list-inline d-sm-block d-none">
                                             <li className="list-inline-item mt-4">
                                                 <Link to=''> <a href="#" target="_blank" className="btn-common "><img src="/projectstarter/banner/pancakeImg.svg" className="img-fluid mr-2 mb-1 pancakeBtn" alt="" /> Buy PSR</a></Link>
@@ -38,7 +39,7 @@ const Banner = () => {
                                             </li>
 
                                         </ul>
-                                        <ul className="list-inline d-sm-block mt-2 d-none">
+                                        {/* <ul className="list-inline d-sm-block mt-2 d-none">
                                             <li className="list-inline-item mt-4">
                                                 <Link to=''><a href="https://www.instagram.com/projectstarter.io" target="_blank" className="btn-common2">Join us Telegram</a></Link>
                                             </li>
@@ -51,13 +52,13 @@ const Banner = () => {
                                             <li className="list-inline-item mt-4">
                                                 <Link to='/rounds'> <a href="#" target="_blank" className="btn-common2">Seed Round<img src="/projectstarter/banner/github.svg" className="img-fluid ml-2 mb-1 pancakeBtn " alt="" /></a></Link>
                                             </li>
-                                        </ul>
+                                        </ul> */}
 
-                                        <div className='d-flex flex-column d-sm-none d-block'>
+                                        {/* <div className='d-flex flex-column d-sm-none d-block'>
                                        <Link to=''> <button href="#" target="_blank" className="btn-common w-100 mt-3"><img src="/projectstarter/banner/pancakeImg.svg" className="img-fluid mr-2 mb-1  pancakeBtn" alt="" /> Buy on PancakeSwap</button></Link>
                                        <Link to='/idoform'><button href="#" target="_blank" className="btn-common w-100 mt-3">Apply for IDO</button></Link>
                                        <Link to='/projects'> <button href="#" target="_blank" className="btn-common w-100 mt-3">View All Projects</button></Link>
-                                       </div>
+                                       </div> */}
                                        <div className='d-flex flex-column mt-5 d-sm-none d-block'>
                                        <Link to=''><button href="#" target="_blank" className="btn-common2 w-100">Join us Telegram</button></Link>
                                        <Link to=''> <button href="#" target="_blank" className="btn-common2 mt-1 w-100">Follow our Twitter</button></Link>
@@ -66,9 +67,29 @@ const Banner = () => {
                                        </div>
 
 
+                                       <ul className='list-inline ptb20 mt-5'>
+                                    <li className='list-inline-item'>
+                                        <div className='inner-user'>
+                                            <img src='\opencanvas-my-profile-assets\binancedex.png' className='img-fluid'></img>
+
+                                        </div>
+
+                                    </li>
+                                    <li className='list-inline-item'>
+                                        <div className='inner-text-r'>
+                                            <h6 className='grey'>BINANCE</h6>
+                                            <p className=''>SMART CHAIN</p>
+                                        </div>
+                                    </li>
+                                </ul>
                                     </div>
                                 </div>
                             </div>
+<div className='col-sm-6'>
+    <div className=''>
+
+    </div>
+</div>
                         </div>
                     </div>
                 </section>
