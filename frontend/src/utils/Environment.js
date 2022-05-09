c73e2ef6345da807df74a45e8a759a2d1ecde891
SardarShahzeb <shahzeb.naseer2@gmail.com>
2022-05-09 17:17:34 +0500

    contract changed

--- frontend/src/utils/Environment.js
+++ frontend/src/utils/Environment.js
@@ -1,7 +1,7 @@
 let Environment = {
     tokenAddress: "0x2aAca69245363661E53d18f35dbc0bCf87Ed1BB8",
     contractAddress: "0x66d2B5B165507c98e10b4aC36b836A56112273dC",
-    deployerContractAddress: "0x40b3047d5436C0De9a2FA0625478D95AfB127880",
-    seedContractAddress: "0xD216053984D26204294e9Ae0458209d5200b2248"
+    deployerContractAddress: "0x90643C0b4265Eae3C610BdE6F43eF05e005fb9bf",
+    seedContractAddress: "0xA4761EfeAFad549fa717A87144697AfC04270556"
   };
   export default Environment;
