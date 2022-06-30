3d94f258eda398d1a85b7ce76fe1f668d74b23fc
abdullah1572 <abdullah.anwar090078@gmail.com>
2022-06-30 18:08:08 +0500

    change

--- frontend/build/static/js/4.6b0f49cb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpopencanvas=this.webpackJsonpopencanvas||[]).push([[4],{970:function(n,p){}}]);
-//# sourceMappingURL=4.6b0f49cb.chunk.js.map
--- frontend/src/utils/Environment.js
+++ frontend/src/utils/Environment.js
@@ -2,6 +2,6 @@ let Environment = {
     tokenAddress: "0x2aAca69245363661E53d18f35dbc0bCf87Ed1BB8",
     contractAddress: "0x66d2B5B165507c98e10b4aC36b836A56112273dC",
     deployerContractAddress: "0x90643C0b4265Eae3C610BdE6F43eF05e005fb9bf",
-    seedContractAddress: "0xA4761EfeAFad549fa717A87144697AfC04270556"
+    seedContractAddress: "0x0A0dA0c556067F206C5fA7a48b949Ed268F6B2B7"
   };
   export default Environment;
